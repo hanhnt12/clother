81483d3f9326a70d2907df313e2042681479b65a
hanhnt12 <nth.hanhnt@gmail.com>
2018-04-03 22:06:41 +0700

    update eslint rule semi and space function

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -24,6 +24,9 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // requires semicolons at the end of statements
+    'semi': 0,
+    'space-before-function-paren': 0
   }
 }
